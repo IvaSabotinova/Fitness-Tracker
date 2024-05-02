ddd4f107720b0da173ae072294497a28937062d1
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2024-05-02 13:38:13 +0300

    Firebase deploy

--- fitness-tracker/src/app/training/training-routing.module.ts
+++ fitness-tracker/src/app/training/training-routing.module.ts
@@ -11,6 +11,6 @@ const routes: Routes = [
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule],
-    providers: [AuthGuard]
+    //providers: [AuthGuard]
 })
 export class TrainingRoutingModule { }
